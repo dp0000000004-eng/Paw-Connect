b5f9bc60d08dea02abab67879424f92c6de8d24a
deba.ai <dp0000000004@gamil.com>
2026-08-14 18:54:34 +0530

    .

--- Campus_Connect/Campus_Connect/settings.py
+++ Campus_Connect/Campus_Connect/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Campus_Connect.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Campus_Connect/exams/views.py
+++ Campus_Connect/exams/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def hy(request):
-    return HttpResponse("Hello from Exams")
+    return render(request, 'exams/exam.html')
