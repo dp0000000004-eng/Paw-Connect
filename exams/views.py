b964e7761ca9673ddcb862aa87f088f0eac2afe7
Rahul <rahulusahu2007@gmail.com>
2026-08-15 23:08:53 +0530

    Clean repo

--- exams/views.py
+++ exams/views.py
@@ -3,9 +3,3 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def hy(request):
-    return render(request, 'exams/exam.html')
-
-
-def rahul():
-    return "This line is made by rahul"
