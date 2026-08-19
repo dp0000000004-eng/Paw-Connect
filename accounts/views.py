386b2c63a2cb85314c6b45680819d6dba3a7a186
deba.ai <dp0000000004@gamil.com>
2026-08-19 16:16:46 +0530

    login function

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
+
     return render(request, 'welcome.html')
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        raw_password = request.POST.get('password')
+
+        user = User(
+            username=username,
+            email=email
+        )
+        user.set_password(raw_password)
+
+        user.save()
+
+        messages.success(
+            request,
+            message="Account Created Successfully"
+        )
+
+    else:
+
+        messages.error(
+            request,
+            message="Something Went Wrong :( "
+        )
