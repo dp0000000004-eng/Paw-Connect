e94bf1eac8db224603c54dd898de6578ec1d68da
deba.ai <dp0000000004@gamil.com>
2026-08-22 22:20:52 +0530

    Login page done almost

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'user'
+
+
 urlpatterns = [
     path('home/', views.home, name="home"),
     path('Create/', views.createAccount, name="create_account"),
+    path('login/', views.login_view, name="login"),
 ]
