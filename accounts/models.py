7ec8bf5ca4655ea1bafd7c42827a6efdb4ad0225
deba.ai <dp0000000004@gamil.com>
2026-08-22 11:26:19 +0530

    change file

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Collage_Meta_Data(models.Model):
+    name = models.CharField(max_length=255)
+    start_in = models.SmallIntegerField(
+        null=True,
+        blank=True
+    )
+    
