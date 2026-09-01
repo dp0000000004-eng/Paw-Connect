450a3c2b084481c76099471b8b6d29b3874a36c9
deba.ai <dp0000000004@gamil.com>
2026-09-01 17:21:06 +0530

    last

--- Campus_Connect/settings.py
+++ Campus_Connect/settings.py
@@ -22,7 +22,7 @@ import os
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 os.environ.get("DEBUG", "False") == "True"
