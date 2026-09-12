ad612ec87ddf364492afaa37bfc98d63d8a1a564
deba.ai <dp0000000004@gamil.com>
2026-09-12 15:11:29 +0530

    serializers done

--- exams/views.py
+++ exams/views.py
@@ -2,6 +2,10 @@
 from django.shortcuts import render
 from .models import Syllabus
 from accounts.models import Departments
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer
+from rest_framework.response import Response
+from .serializers import SyllabusSerializer
 
 
 def departments(request):
@@ -16,15 +20,16 @@ def departments(request):
         }
     )
 
-
+@api_view(['GET', 'POST'])
+@renderer_classes([TemplateHTMLRenderer, JSONRenderer])
 def syllabus(request, branch_id):
     # Only syllabus records that belong to this department / branch
     syllabuses = Syllabus.objects.filter(branch=branch_id)
+    syllabusesSerializer = SyllabusSerializer(syllabuses, many=True)
 
-    return render(
-        request,
-        'exams/links.html',
+    return Response(
         {
-            "syllabuses": syllabuses
-        }
+            "syllabuses": syllabusesSerializer.data
+        },
+        template_name="exams/links.html"
     )
